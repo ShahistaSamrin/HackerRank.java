9a67b0a83fda47df5c060c985cd2f7b00ca58f39
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-18 22:11:36 +0530

    Add files via upload

---
+++ Reversestring.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Reversestring{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String A=sc.nextLine().trim();
+        String B=new
+        StringBuilder(A).reverse().toString();
+        if(A.equals(B))
+        {
+        System.out.println("Yes");
+        }
+        else
+        {
+        System.out.println("No");
+        }
+    }
+}
